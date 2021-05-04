44688a887950c7b00a17aba48983a419ddfd8a12
Burcu ŞENOL <burcusenol62@gmail.com>
2021-05-05 02:49:01 +0300

    05.05.2021

---
+++ abstractClasses/src/abstractClasses/Main.java
@@ -0,0 +1,10 @@
+package abstractClasses;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//GameCalculator gameCalculator=new WomanGameCalculator();
+	}
+
+}
---
+++ abstractClasses/src/abstractClasses/ManGameCalculator.java
@@ -0,0 +1,12 @@
+package abstractClasses;
+
+public class ManGameCalculator extends GameCalculator {
+
+	@Override
+	public void hesapla() {
+		System.out.println("Puanýnýz : 95");
+		
+	}
+	
+	
+}
